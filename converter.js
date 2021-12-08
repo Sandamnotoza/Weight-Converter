ce1cdc8707fc7307310a95e3a11fc9d62a6595dd
Sandamnotoza <90473088+Sandamnotoza@users.noreply.github.com>
2021-12-08 13:13:46 +0200

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,16 @@
+document.getElementById('btn').addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    let kgs = parseInt(document.getElementById('kgsInput').value);
+    
+  
+    document.getElementById('poundsOutput').innerHTML = kgs * 2.2047058824;
+    document.getElementById('ouncesOutput').innerHTML = kgs * 35,2739215686;
+    document.getElementById('gramsOutput').innerHTML = kgs * 1000;
+
+    
+});
+
+
+
